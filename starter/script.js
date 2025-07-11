ac2a2729f3e41d6254d39ff8995dee4a416e12fe
Vishalrp02 <vishalrajpurohit02>
2025-07-11 12:02:16 +0530

    initial

--- starter/script.js
+++ starter/script.js
@@ -1,13 +1,4 @@
 'use strict';
-/*console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
@@ -65,26 +56,4 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-  //When guess is too high
-  //  else if (guess > secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent = 'Too High';
-  //       score--;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = 'You lost the game';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //   }
-  //   //When guess is too low
-  //   else if (guess < secretNumber) {
-  //     if (score > 1) {
-  //       document.querySelector('.message').textContent = 'Too Low';
-  //       score--;
-  //       document.querySelector('.score').textContent = score;
-  //     } else {
-  //       document.querySelector('.message').textContent = 'You lost the game';
-  //       document.querySelector('.score').textContent = 0;
-  //     }
-  //   }
 });
